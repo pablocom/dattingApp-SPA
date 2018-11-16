a59bc599227e057dc1fb7e42b294b4df0ec80512
Pablo Company <p.c.ramirez5150@gmail.com>
2018-11-16 11:55:06 +0100

    commit con el georgi

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,29 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  jwtHelper = new JwtHelperService();
+  baseUrl = 'http://localhost:5000/api/auth/';
+  decodedToken: any;
 
-constructor(private http: HttpClient) { }
 
-baseUrl = 'http://localhost:5000/api/auth/';
 
-login(model: any) {
-  return this.http.post(this.baseUrl + 'login', model).pipe(
-    map((response: any) => {
-      const user = response;
-      if (user) {
-        localStorage.setItem('token', user.token);
-      }
-    })
-  );
-}
+  constructor(private http: HttpClient) { }
 
-register(model: any) {
-  return this.http.post(this.baseUrl + 'register', model);
-}
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          console.log(this.decodedToken);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token); // devolvera falso si no hay token o ha expirado
+  }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 
 @Component({
   selector: 'app-root',
@@ -6,6 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  title = 'DattingApp-SPA';
+export class AppComponent implements OnInit {
+  jwtHelper = new JwtHelperService();
+
+  constructor(private authService: AuthService) {
+
+  }
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authService.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 }
