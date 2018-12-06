18b743b00e4825d6bae8455ffcc4dbf7a4a07785
pablocom <p.c.ramirez5150@gmail.com>
2018-12-06 17:56:12 +0100

    haciendo mucha trampa

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,6 +7,7 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 export const appRoutes: Routes = [
     {path: 'home', component: HomeComponent},
@@ -19,6 +20,7 @@ export const appRoutes: Routes = [
                 resolve: {users: MemberListResolver}},
             {path: 'members/:id', component: MemberDetailComponent,
                 resolve: {user: MemberDetailResolver}},
+            {path: 'member/edit', component: MemberEditComponent},
             {path: 'messages', component: MessagesComponent},
             {path: 'lists', component: ListsComponent},
         ]
