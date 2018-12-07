284b9d393558af1e2be76359b45b53ef04a84c45
pablocom <p.c.ramirez5150@gmail.com>
2018-12-07 14:39:37 +0100

    end of section 10

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -25,4 +25,8 @@ export class UserService {
   getUser(id): Observable<User> {
     return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
   }
+
+  updateUser(id, user: User): Observable<User> {
+    return this.http.put<User>(this.baseUrl + 'users/' + id, user, httpOptions);
+  }
 }
